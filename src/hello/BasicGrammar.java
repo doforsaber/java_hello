dce1b58abab385405ec04000ed1be271bdfa6454
cowl <doforsaber@126.com>
2018-04-08 21:31:07 +0800

    hello world

---
+++ src/hello/BasicGrammar.java
@@ -0,0 +1,20 @@
+package hello;
+
+public class BasicGrammar {
+	/*
+	## Java 基础语法
+	一个java程序可以认为是系列对象的集合
+	对象: 类的是咧,有状态和行为
+	类: 模板 描述类对象的状态和行为
+	方法: 行为
+	实例变量: 每个对象都有独特的实例变量 
+	 */
+	
+	/*
+	## 第一个Java程序
+	输出Hello World
+	*/
+	public static void main(String []args) {
+		System.out.println("Hello World!"); //打印 Hello World
+	}
+}
